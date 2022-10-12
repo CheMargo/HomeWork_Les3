0c003916573f312b0b9bf3efe63d64ed1b80d718
Margo <ritosnsk@gmail.com>
2022-10-12 23:40:36 +0600

    решение задачи 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -5,3 +5,35 @@
 14212 -> нет
 12821 -> да
 23432 -> да*/
+
+string Reverse( string s )
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+bool isPalindrome(string s)
+{
+    string reversedS = Reverse(s);
+    if (s == reversedS)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("введите пятизначное число");
+string N = Console.ReadLine();
+if (isPalindrome(N) == true)
+{
+    Console.WriteLine(N + " -> является палиндромом");
+}
+else
+{
+    Console.WriteLine(N + " -> не является палиндромом");
+ 
+}
